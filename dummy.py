76aa100c3ae2d3493e0e3a5d1bae07ff8441ec07
harrish1709 <150880592+harrish1709@users.noreply.github.com>
2025-07-10 22:08:15 +0530

    Delete dummy.py

--- dummy.py
+++
@@ -1,9 +0,0 @@
-import os
-
-db_path = "governance.db"
-
-if os.path.exists(db_path):
-    os.remove(db_path)
-    print("✅ Database deleted successfully.")
-else:
-    print("❌ Database file not found.")
