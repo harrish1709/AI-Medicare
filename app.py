9138ad0e6dbed41f045a2a4ec1621507ee523a18
harrish1709 <150880592+harrish1709@users.noreply.github.com>
2025-11-18 16:35:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.secret_key = os.urandom(24)
 # openai.api_key = "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"
 # openai.api_base = "https://api.groq.com/openai/v1"
 client = Groq(
-    api_key="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"
+    api_key=os.getenv("GROQ_API_KEY")
 )
 
 # --- Constants ---
